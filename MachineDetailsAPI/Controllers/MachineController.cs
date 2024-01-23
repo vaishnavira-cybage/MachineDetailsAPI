a0dbf8053fe2a018d1a34436af2b20fde8d534a7
Vaishnavi <vaishnavira@cybage.com>
2024-01-23 14:52:05 +0530

    first commit

---
+++ MachineDetailsAPI/Controllers/MachineController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json.Serialization;
+
+namespace MachineDetailsAPI.Controllers
+{
+        [ApiController]
+        [Route("[controller]")]
+        public class DetailsController : ControllerBase
+        {
+            [HttpGet]
+            public IActionResult GetMachineDetails()
+            {
+                var machineName = Environment.MachineName;
+                var machineIP = Dns.GetHostAddresses(Dns.GetHostName())[0].ToString();
+                var machineOS = Environment.OSVersion.ToString();
+                var timestamp = DateTime.Now.ToString();
+
+                var machineDetails = new
+                {
+                    MachineName = machineName,
+                    MachineIP = machineIP,
+                    MachineOS = machineOS,
+                    Timestamp = timestamp
+                };
+
+                return Ok(machineDetails);
+            }
+        }
+
+}
+
